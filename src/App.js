16b60eb95a28d3cddc268e4c2666342c44798d00
Jorge <jim_2306@hotmail.com>
2021-03-29 22:15:03 -0500

    second

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Mensaje from "./mensaje";
 function App() {
   return (
     <div className="App">
-      <Mensaje color={"green"} message={"Hola Jorge"} />
+      <Mensaje color={"green"} message={"Hola Jorge Martinez"} />
       <Mensaje color={"black"} message={"Esto es React"} />
       <Mensaje color={"yellow"} message={"Lo que Aprendere al 100%"} />
     </div>
